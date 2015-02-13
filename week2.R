86de069ca2918039c648d410c83ec11483fff742
Risto Kaartinen <risto.kaartinen@gmail.com>
2015-02-13 16:39:37 +0200

    Viimeinen tehtävä uupuu.

---
+++ week2.R
@@ -0,0 +1,65 @@
+#q1
+library(AppliedPredictiveModeling)
+library(caret)
+data(AlzheimerDisease)
+
+
+#Vastaus
+
+adData = data.frame(diagnosis,predictors)
+trainIndex = createDataPartition(diagnosis, p = 0.50,list=FALSE)
+training = adData[trainIndex,]
+testing = adData[-trainIndex,]
+
+
+#Q2
+
+library(AppliedPredictiveModeling)
+data(concrete)
+library(caret)
+set.seed(975)
+inTrain = createDataPartition(mixtures$CompressiveStrength, p = 3/4)[[1]]
+training = mixtures[ inTrain,]
+testing = mixtures[-inTrain,]
+
+featurePlot(x= training  %>% select())
+
+#q3
+
+library(AppliedPredictiveModeling)
+data(concrete)
+library(caret)
+set.seed(975)
+inTrain = createDataPartition(mixtures$CompressiveStrength, p = 3/4)[[1]]
+training = mixtures[ inTrain,]
+testing = mixtures[-inTrain,]
+
+#q4
+
+library(caret)
+library(AppliedPredictiveModeling)
+set.seed(3433)
+data(AlzheimerDisease)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+
+x <- training  %>% select(grep("^IL",names(training)))
+x<- preProcess(x, method = "pca", thresh = 0.9)
+
+#q5
+
+library(caret)
+library(AppliedPredictiveModeling)
+set.seed(3433)
+data(AlzheimerDisease)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+
+training <- training  %>% select(diagnosis,grep("^IL",names(training)))
+
+x<- preProcess(training, method = "pca", thresh = 0.8)
+
